6f6eab7919364836dc9915bd8f5ffe01b38c7924
manhvu <manh.vv.htth@gmail.com>
2017-09-21 16:43:19 +0700

    add home-page

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import './index.css';
 import './index.scss';
-import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import { store } from './config/Store';
 
