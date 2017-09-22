81855e5e9fecfff825aa28f5320f694dc8c6108e
manhvu <manh.vv.htth@gmail.com>
2017-09-22 13:18:46 +0700

    add security config

--- back-end/src/main/java/lecle/nanum/config/WebSecurityConfig.java
+++ back-end/src/main/java/lecle/nanum/config/WebSecurityConfig.java
@@ -4,12 +4,20 @@ package lecle.nanum.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.security.SecureRandom;
@@ -18,6 +26,9 @@ import java.security.SecureRandom;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public static final String SIGNING_KEY = "3XwKDYUyPTHXlfeZ";
+    private static final String SECURITY_REALM = "Spring Boot JWT nanum Realm";
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -30,10 +41,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // no cache at all
-        http.headers().cacheControl();
-        http.csrf().disable();
-        http.sessionManagement().disable();
+        http.sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .csrf().disable();
 
         http
                 .authorizeRequests()
@@ -43,22 +54,59 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anonymous()
 
                 .and()
+                .httpBasic().realmName(SECURITY_REALM)
+
+                .and()
                 .formLogin()
 //                .loginPage("/login")
                 .defaultSuccessUrl("/", false)
                 .permitAll()
+
                 .and()
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout", HttpMethod.GET.name()))
                 .logoutSuccessUrl("/")
                 .clearAuthentication(true)
-                .and()
         ;
 
     }
 
     @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("1234").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("ADMIN");
+    }
+
+    @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return (new BCryptPasswordEncoder(12, new SecureRandom("lecle.co.kr-nanum".getBytes())));
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(SIGNING_KEY);
+        return converter;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
     }
 }
--- back-end/src/main/java/lecle/nanum/controller/HomeController.java
+++ back-end/src/main/java/lecle/nanum/controller/HomeController.java
@@ -2,6 +2,7 @@ package lecle.nanum.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by manhvu on 9/20/17.
@@ -14,4 +15,9 @@ public class HomeController {
         return "index";
     }
 
+    @GetMapping(value = "/health-check/private")
+    @ResponseBody
+    public String healthCheckPrivate() {
+        return "health check private success";
+    }
 }
