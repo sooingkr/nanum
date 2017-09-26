054af2fb49934ae93fe1986bd56c2a531f80afab
Phat G Ha <phatg@lecle.co.kr>
2017-09-26 16:26:17 +0700

    Move mock data to each child container

--- client/src/containers/Dashboard/Dashboard.js
+++ client/src/containers/Dashboard/Dashboard.js
@@ -4,57 +4,29 @@ import {
   Row,
   Col,
 } from 'react-bootstrap';
+import UserInfoContainer from './UserInfoContainer';
+import FoodIntakeTrackingContainer from './FoodIntakeTrackingContainer';
 import FoodSuggestionContainer from './FoodSuggestionContainer';
 import IngredientsSuggestionContainer from './IngredientsSuggestionContainer';
-
-const mockIngredientsSuggestion = {
-  heading: "Today's recommendation",
-  recommendation: "Mineral intake recommended",
-  ingredients: [
-    { name: "Tomato" },
-    { name: "Carrot" },
-    { name: "Spinach" },
-  ]
-};
-
-const mockFoodSuggestions = [
-  {
-    id: "1234",
-    name: "chiecken",
-    imageUrl: "http://via.placeholder.com/200x150",
-  },
-  {
-    id: "123",
-    name: "chiecken 1",
-    imageUrl: "http://via.placeholder.com/200x150",
-  },
-  {
-    id: "14",
-    name: "chiecken 2",
-    imageUrl: "http://via.placeholder.com/200x150",
-  },
-  {
-    id: "155",
-    name: "chiecken 3",
-    imageUrl: "http://via.placeholder.com/200x150",
-  },
-  {
-    id: "14545",
-    name: "chiecken 4",
-    imageUrl: "http://via.placeholder.com/200x150",
-  }
-];
 
 class Dashboard extends Component {
   render() {
     return (
       <Grid>
         <Row>
+          <Col sm={6} md={4}>
+            <UserInfoContainer />
+          </Col>
+          <Col sm={6} md={8}>
+            <FoodIntakeTrackingContainer />
+          </Col>
+        </Row>
+        <Row>
           <Col sm={6} md={2}>
-            <IngredientsSuggestionContainer data={mockIngredientsSuggestion}/>
+            <IngredientsSuggestionContainer />
           </Col>
           <Col sm={6} md={10}>
-            <FoodSuggestionContainer data={mockFoodSuggestions}/>
+            <FoodSuggestionContainer />
           </Col>
         </Row>
       </Grid>
