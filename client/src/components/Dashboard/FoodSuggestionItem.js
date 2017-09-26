2833fd8e0d5b17d8644d4e8b619fec7dce478941
Phat G Ha <phatg@lecle.co.kr>
2017-09-26 16:26:39 +0700

    Replace img with Image from react-bootstrap

--- client/src/components/Dashboard/FoodSuggestionItem.js
+++ client/src/components/Dashboard/FoodSuggestionItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Image } from 'react-bootstrap';
 
 const FoodSuggestionItem = ({ data: { id, imageUrl, name } }) => (
   <div className="food-suggestion-item" id={id}>
     <figure className="food-suggestion-item__image">
-      <img src={imageUrl} alt={name} />
+      <Image src={imageUrl} alt={name} responsive />
     </figure>
     <p className="food-suggestion-item__name">{name}</p>
   </div>
