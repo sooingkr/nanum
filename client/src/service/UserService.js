61f262e6ea16681ae1a8cafa3be74401bb2274b9
Phat G Ha <phatg@lecle.co.kr>
2017-09-29 14:19:46 +0700

    Add UserService

--- client/src/service/UserService.js
+++ client/src/service/UserService.js
@@ -1,8 +1,35 @@
-import {server} from '../config/server';
+import axios from 'axios';
+import { API_BASE_URL } from '../constants/api';
+import initializeMockAPI from './mockAPI/api';
 
-const apiPath = '/api/user';
+if (process.env.NODE_ENV === 'development') {
+  initializeMockAPI(axios);
+}
 
-export const userService = {
-  getUserById: userId => server.get(`${apiPath}/${userId}`),
-  getCurrentUser: () => server.get(`${apiPath}/1`), /* TODO fix get current user from login store */
-};
+export const getCurrentUser = async () => {
+  let currentUser;
+
+  try {
+    currentUser = await axios.get(`${API_BASE_URL}/user/current`);
+  } catch(error) {
+    throw new Error(`UserService error - <getCurrentUser()>: ${error}`);
+  }
+  return currentUser;
+}
+
+export const getFoodIntakeTracking = async (userId) => {
+  let intakeTracking;
+  
+  try {
+    intakeTracking = await axios.get(`${API_BASE_URL}/tracking`, {
+      params: { userId }
+    });
+  } catch(error) {
+    throw new Error(`UserService error - <getFoodIntakeTracking()>: ${error}`);
+  }
+  return intakeTracking;
+}
+
+export const loginUser = async () => {
+  // TODO
+}
