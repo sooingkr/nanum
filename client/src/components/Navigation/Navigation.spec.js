71f290607ad9947c78e73a89ea491f33d79c7a1f
Yen Hua <yenhua@orgos.net>
2017-10-25 09:54:06 +0700

    - Update Nav test

--- client/src/components/Navigation/Navigation.spec.js
+++ client/src/components/Navigation/Navigation.spec.js
@@ -3,7 +3,15 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import sinon from 'sinon';
 
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Image,
+} from "react-bootstrap";
+
 import Navigation from "../Common/Navigation";
+import FoodSearchBoxContainer from '../../containers/FoodSearch/FoodSearchBoxContainer';
 
 describe('<Navigation /> component', () => {
 
@@ -19,100 +27,78 @@ describe('<Navigation /> component', () => {
     expect(wrapper.find('header').length).toBe(1);
   });
 
-  describe('<nav>', () => {
+  describe('<Navigation /> wrapper', () => {
+
+    const navWrapper = wrapper.children().find(Navbar);
     it('should render <nav> tag ', () => {
-      expect(wrapper.children().find('nav').length).toBe(1);
+      expect(navWrapper.length).toBe(1);
     });
 
-    const navContainer = wrapper.find('nav').children();
+    describe('div class="navigation__logo" wrapper', () => {
 
-    describe('div class="navigation__logo" ', () => {
-      it('should render div class="navigation__logo" ', () => {
-        expect(navContainer.find('.navigation__logo').length).toBe(1);
+      it('should render div class="navigation__logo"', () => {
+        expect(navWrapper.children().find('.navigation__logo').length).toBe(1);
       });
 
-      it('should render div class="logo" ', () => {
-        expect(navContainer.find('.logo').length).toBe(1);
+      const logoWrapper = navWrapper.children().find('.logo');
+
+      it('should render div class="logo"', () => {
+        expect(logoWrapper.length).toBe(1);
       });
 
-      it('should render 2 images in div class="logo" ', () => {
-        expect(navContainer.find('img').length).toBe(2);
+      it('should render 2 logo images', () => {
+        expect(logoWrapper.children().find(Image).length).toBe(2);
       });
 
-      const toggleButton = navContainer.find('.navigation__toggle');
-
-      it('should render a toggle nav button mobile', () => {
-        expect(toggleButton.length).toBe(1);
+      it('should render div class="navigation__toggle"', () => {
+        expect(navWrapper.children().find(".navigation__toggle").length).toBe(1);
       });
 
-      it('click to toggle menu on mobile', () => {
-        toggleButton.simulate('click');
-        expect(handleClick.calledOnce).toEqual(true);
-      });
     });
 
-    describe('div class="navigation__main" ', () => {
+    describe('div class="navigation__main" wrapper', () => {
+
+      const navMain = navWrapper.children().find('.navigation__main');
       it('should render div class="navigation__main"', () => {
-        expect(navContainer.find('.navigation__main').length).toBe(1);
+        expect(navMain.length).toBe(1);
       });
 
-      it('should render class="collapse"', () => {
-        expect(navContainer.find('.collapse').length).toBe(1);
+      const navLink = navMain.children().find(Nav);
+
+      it('should render list <ul class="nav"></ul>', () => {
+        expect(navLink.length).toBe(1);
       });
 
-      it('should render <ul class="collapse nav navbar-nav navbar-right"></ul> list', () => {
-        navContainer.find('.collapse > li').forEach((node, index) => {
-          expect(node.find('<li>').length).toBeGreaterThanOrEqual(1);
+      it('should render a list navlink"', () => {
+        navLink.find('li').forEach((node) => {
+          expect(node.length).toBeGreaterThanOrEqual(6);
         });
       });
     });
 
-    describe('div class="navigation__secondary" ', () => {
+    describe('div class="navigation__secondary" wrapper', () => {
+
+      const navSecond = navWrapper.children().find('.navigation__secondary');
+
       it('should render div class="navigation__secondary"', () => {
-        expect(navContainer.find('.navigation__secondary').length).toBe(1);
+        expect(navSecond.length).toBe(1);
       });
 
-      it('should render <ul class="nav"></ul>', () => {
-        expect(navContainer.find('.nav').length).toBe(1);
-      });
+      describe('<FoodSearchBoxContainer> component', () => {
 
-      describe('div class="food-search__box" ', () => {
-
-        it('should render <div class="food-search__box"></div>', () => {
-          expect(navContainer.find('.food-search__box').length).toBe(1);
+        it('should render <FoodSearchBoxContainer>', () => {
+          expect(navSecond.find(FoodSearchBoxContainer).length).toBe(1);
         });
 
-        const foodSearchBox = navContainer.find('.food-search__box');
+        const navDropDown = navSecond.find(NavDropdown);
 
-        it('should render <div class="search-form search-form--light"></div>', () => {
-          expect(foodSearchBox.children().find('.search-form').length).toBe(1);
+        it('should render NavDropdown', () => {
+          expect(navDropDown.length).toBe(1);
         });
 
-        it('should render <form> element', () => {
-          expect(foodSearchBox.children().find('form').length).toBe(1);
-        });
-
-        it('should render <input> element', () => {
-          expect(foodSearchBox.children().find('input[type="text"]').length).toBe(1);
-        });
-
-        it('should render <button> element', () => {
-          expect(foodSearchBox.children().find('button').length).toBeGreaterThanOrEqual(1);
-        });
-      });
-
-      describe('div class="dropdown" ', () => {
-        it('should render div class="dropdown"', () => {
-          expect(navContainer.children().find('.dropdown').length).toBe(1);
-        });
-
-        it('should render a id="secondary-nav-dropdown"', () => {
-          expect(navContainer.children().find('#secondary-nav-dropdown').length).toBe(1);
-        });
-
-        it('should render <ul class="dropdown-menu"></ul> list', () => {
-          navContainer.find('.dropdown-menu > li').forEach((node, index) => {
-            expect(node.find('<li>').length).toBeGreaterThanOrEqual(1);
+        it('should render a group user menu"', () => {
+          navDropDown.find('li').forEach((node) => {
+            expect(node.length).toBeGreaterThanOrEqual(2);
           });
         });
       });
