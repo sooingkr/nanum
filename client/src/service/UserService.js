5655216bdfeb8cfd3bd939d37d691608a3fc8392
Phat G Ha <phatg@lecle.co.kr>
2017-11-07 09:53:44 +0700

    Remove unused api #79

--- client/src/service/UserService.js
+++ client/src/service/UserService.js
@@ -1,40 +1,5 @@
 import axios from './config';
-import jwtDecode from 'jwt-decode';
-import { saveAuth } from '../utils/auth';
 import { API_BASE_PATH } from '../constants';
-import { stringify } from 'querystring';
-
-const decodeUserToken = token => {
-  return jwtDecode(token);
-};
-
-const loginUser = async (email, password) => {
-  let token;
-  let decodedToken;
-
-  try {
-    token = await axios.post(`${API_BASE_PATH}/post-login`, stringify({username: email, password}));
-    decodedToken = decodeUserToken(token.data);
-
-    // Save auth to localstorage
-    saveAuth(decodedToken.id, token.data);
-  } catch(error) {
-    throw new Error(`UserService error - <loginUser()>: ${error}`);
-  }
-  
-  // decode jwt
-  return decodeUserToken(token.data);
-};
-
-const checkValidToken = async (token) => {
-  const isValid = await axios.post(`${API_BASE_PATH}/check-valid-authorization-token`, {
-      headers: {
-        'Authorization': token,
-      }
-    });
-
-  return isValid.data;
-};
 
 // DailyReport
 const getTrackingData = async (queryTime) => {
@@ -54,7 +19,4 @@ const getTrackingData = async (queryTime) => {
 
 export default {
   getTrackingData,
-  loginUser,
-  checkValidToken,
-  decodeUserToken,
 }
