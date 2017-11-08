9324eea826e0d932c4e4a8c3a8eb31dc32651647
navcs <nguyenanhvu.cs@gmail.com>
2017-11-08 18:14:49 +0700

    - add server to ec2

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -2,7 +2,7 @@
 const isProd = (process.env.NODE_ENV === 'production');
 
 console.log("---- is prod:", isProd);
-export const API_BASE_PATH = isProd ? '/nanum-api' : 'http://test.baikal.io:8080';
+export const API_BASE_PATH = isProd ? '/nanum-api' : 'http://13.229.93.252:8080';
 
 // auth
 export const STORAGE_ID = 'user_id';
