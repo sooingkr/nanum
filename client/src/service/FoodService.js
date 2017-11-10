ec20db6b41c00c1728ec9e6c192740ec906b27dc
navcs <nguyenanhvu.cs@gmail.com>
2017-11-10 14:14:51 +0700

    - Fix service base url

--- client/src/service/FoodService.js
+++ client/src/service/FoodService.js
@@ -9,7 +9,7 @@ const searchFood = async (query, page=0, size=20, sort='createTime,asc') => {
   let response;
 
   try {
-    response = await axios.get(`${API_BASE_PATH}/foods/search`, {
+    response = await axios.get(`/foods/search`, {
       params: {
         query,
         page,
@@ -28,7 +28,7 @@ const suggestFood = async (query) => {
   let results;
 
   try {
-    results = await axios.get(`${API_BASE_PATH}/foods/suggest`, {
+    results = await axios.get(`/foods/suggest`, {
       params: { query }
     });
   } catch(error) {
@@ -42,7 +42,7 @@ const suggestFood = async (query) => {
 const foodDetail = async (foodId) => {
 
   try {
-    const result =  await axios.get(`${API_BASE_PATH}/foods/details/${foodId}`).then(res => res.data);
+    const result = await axios.get(`/foods/details/${foodId}`).then(res => res.data);
     return result;
 
   } catch(error) {
@@ -54,7 +54,7 @@ const removeFoods = async (foods) => {
   let result;
 
   try {
-    result = await axios.post(`${API_BASE_PATH}/foods/intake/delete`, {
+    result = await axios.post(`/foods/intake/delete`, {
       data: { foods }
     });
   } catch (error) {
