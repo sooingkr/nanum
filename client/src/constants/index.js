1cc51775e1def8a6294de492a14f732d9cbf19ee
navcs <nguyenanhvu.cs@gmail.com>
2017-11-10 14:24:20 +0700

    - Fix service base url

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -2,7 +2,7 @@
 const isProd = (process.env.NODE_ENV === 'production');
 
 console.log("---- is prod:", isProd);
-export const API_BASE_PATH = isProd ? '/' : 'http://192.168.0.102:8080/nanum-api';
+export const API_BASE_PATH = isProd ? '/nanum-api' : 'http://192.168.0.102:8080/nanum-api';
 
 // auth
 export const STORAGE_ID = 'user_id';
