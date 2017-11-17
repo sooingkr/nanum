5e7caec3b3e5bf293aedd9fa28728834d1634f06
Phat G Ha <phatg@lecle.co.kr>
2017-11-17 13:28:55 +0700

    Change dev server endpoint #113

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,8 +1,7 @@
 // api
 const isProd = (process.env.NODE_ENV === 'production');
 
-// export const API_BASE_PATH = isProd ? '/nanum-api' : 'http://192.168.0.102:8080/nanum-api'; // server by lecle
-export const API_BASE_PATH = isProd ? '/recommendation-api' : 'http://192.168.0.26:8080/recommendation-api'; // server by navcs
+export const API_BASE_PATH = isProd ? '/recommendation-api' : 'http://192.168.0.102:8080/recommendation-api'; // server by navcs
 
 // auth
 export const STORAGE_ID = 'user_id';
