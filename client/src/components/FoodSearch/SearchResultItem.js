0c2238e83997e72333d03591bcdd39be769fa992
Phat G Ha <phatg@lecle.co.kr>
2017-11-20 14:34:00 +0700

    Fix search result #117

--- client/src/components/FoodSearch/SearchResultItem.js
+++ client/src/components/FoodSearch/SearchResultItem.js
@@ -5,20 +5,16 @@ import { Image } from 'react-bootstrap';
 
 const SearchResultItem = ({ id, name, manufacturer, imageUrl }) => (
   <div className="search-result-item">
-    <div className="search-result-item__image">
-      <Image src={imageUrl} responsive />
-    </div>
-    <div className="search-result-item__content">
-      <h3 className="search-result-item__name">
-        <Link to={'/foods/' + id}>
-          {name}
-        </Link>
-      </h3>
-      <h4 className="search-result-item__manufacturer">{manufacturer}</h4>
-    </div>
-    <div className="search-result-item__button">
-      <Link to={'/foods/' + id} className="button button--arrow"/>
-    </div>
+    <Link to={'/foods/' + id}>
+      <div className="search-result-item__image">
+        <Image src={imageUrl} />
+      </div>
+      <div className="search-result-item__content">
+        <h3 className="search-result-item__name">{name}</h3>
+        <h4 className="search-result-item__manufacturer">{manufacturer}</h4>
+      </div>
+      <div className="search-result-item__button button button--arrow"></div>
+    </Link>
   </div>
 )
 
