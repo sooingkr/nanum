714a8a45946ca69cc531a92e253f1579607d9710
Phat G Ha <phatg@lecle.co.kr>
2017-11-27 17:05:58 +0700

    Fix NoInfo url

--- client/src/components/Dashboard/NoInfo.js
+++ client/src/components/Dashboard/NoInfo.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const NoInfo = (conditionFn, defaultProps={}, label, link) => (Component) => (props) => {
   return conditionFn(props) 
   ? (
     <div className="no-info">
       <Component {...props} {...defaultProps} />
-      <Link to={link} className="no-info__message">
+      <a href={link} className="no-info__message">
         <p>{label}</p>
-      </Link>
+      </a>
       <div className="no-info__overlay"></div>
     </div>
   )
