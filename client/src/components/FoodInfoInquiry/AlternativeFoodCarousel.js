3771377b8f0f86b3cf2f048d1980ea614aea48e8
navcs <nguyenanhvu.cs@gmail.com>
2017-12-01 16:51:19 +0700

    - Fix product detail

--- client/src/components/FoodInfoInquiry/AlternativeFoodCarousel.js
+++ client/src/components/FoodInfoInquiry/AlternativeFoodCarousel.js
@@ -4,8 +4,27 @@ import { isArray, isEmpty } from 'lodash';
 import Carousel from '../Common/Carousel';
 import AlternativeFoodItem from './AlternativeFoodItem';
 import NoInfo from '../Dashboard/NoInfo';
-import { mockFoodDetails } from '../../service/mockAPI/responses';
+import AlternativeFood1 from '../../assets/images/alternative-food-1.png';
+import AlternativeFood2 from '../../assets/images/alternative-food-2.png';
 
+const mockFoodDetails = {
+  name: "캔모아딸기아이스크림",
+  imageUrl: "https://images.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+  diagnosticMessage: "임산부인 김레클 님에게 좋지 않은 비타민 c 와 비타민 e 가 다량 함유된 제품입니다. 김레클 님에게 추천하지 않습니다.",
+  calories: 1200,
+  proteins: 102,
+  carbohydrates: 340,
+  fat: 201,
+  alternativeFoods: [{
+    id: "d3b8d7ba-c451-11e7-abc4-cec278b6b50a",
+    imageUrl: AlternativeFood1,
+    alternativeReason: "철분과 비타민A 가 함유된 HACCP 인증 식품인 <a href='/recommendation/#/foods/2'>요거트 그릭</a>을 대체식품으로 추천합니다."
+  }, {
+    id: "28146c5c-c452-11e7-abc4-cec278b6b50a",
+    imageUrl: AlternativeFood2,
+    alternativeReason: "철분과 비타민A 가 함유된 HACCP 인증 식품인 <a href='/recommendation/#/foods/2'>요거트 그릭</a>을 대체식품으로 추천합니다."
+  }]
+}
 const foodCarouselSettings = {
   arrows: true,
   dots: false,
