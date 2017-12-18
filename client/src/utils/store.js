3114bfa09ea4b0bc8ffce419fe9dac05bae6db54
Phat G Ha <phatg@lecle.co.kr>
2017-12-18 14:40:39 +0700

    Remove console.log from client (#193)

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,11 +1,6 @@
 export const isProd = process.env.NODE_ENV === 'production';
 export const notProd = !isProd;
 
-export const NOPE_FN = (...args) => {
-  if (notProd) {
-    console.log('-- NOPE_FN: ', args[0]);
-  }
-};
 export const createAction = (type, payload) => ({ type, payload });
 
 export const createReducer = (initialState, handlers) => (state = initialState, action) => {
