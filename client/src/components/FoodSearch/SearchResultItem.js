56de2689747fbbd1dad42ff5bba2ff8e23927fe8
navcs <nguyenanhvu.cs@gmail.com>
2018-01-04 14:02:27 +0700

    #217: - Add log for debugger

--- client/src/components/FoodSearch/SearchResultItem.js
+++ client/src/components/FoodSearch/SearchResultItem.js
@@ -2,17 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const SearchResultItem = ({ id, name, manufacturer, imageUrl }) => (
-  <div className="search-result-item">
-    <Link to={'/foods/' + id} style={{ background: `url(${imageUrl})`, backgroundSize: 'cover' }} onError={(e) => e.target.src='http://via.placeholder.com/350x150'}>
-      <div className="search-result-item__content">
-        <h3 className="search-result-item__name">{name}</h3>
-        <h4 className="search-result-item__manufacturer">{manufacturer}</h4>
-      </div>
-      <div className="search-result-item__button button button--arrow"></div>
-    </Link>
-  </div>
-)
+const SearchResultItem = ({id, name, manufacturer, imageUrl}) => {
+  console.log('SearchResultItem');
+  return (
+    <div className="search-result-item">
+      <Link to={'/foods/' + id} style={{background: `url(${imageUrl})`, backgroundSize: 'cover'}}
+            onError={(e) => e.target.src = 'http://via.placeholder.com/350x150'}>
+        <div className="search-result-item__content">
+          <h3 className="search-result-item__name">{name}</h3>
+          <h4 className="search-result-item__manufacturer">{manufacturer}</h4>
+        </div>
+        <div className="search-result-item__button button button--arrow"></div>
+      </Link>
+    </div>
+  )
+}
 
 SearchResultItem.propTypes = {
   id: PropTypes.string.isRequired,
